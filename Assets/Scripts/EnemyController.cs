e1ca854619e42e642194517097f976bdb11ff19c
Yilan He <yilan.he1107@gmail.com>
2015-04-29 20:13:37 +1000

    added ZombieConga Project

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+	public float speed = -1;
+	private Transform spawnPoint;
+	// Use this for initialization
+	void Start () {
+		FixedUpdate ();
+		spawnPoint = GameObject.Find ("SpawnPoint").transform;
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void FixedUpdate() {
+		GetComponent<Rigidbody2D>().velocity = new Vector2 (speed, 0);
+	}
+	void OnBecameInvisible()
+	{
+		if (Camera.main == null)
+			return;
+		float yMax = Camera.main.orthographicSize - 0.5f;
+		transform.position = new Vector3 (spawnPoint.position.x,
+		                                 Random.Range (-yMax, yMax),
+		                                 transform.position.z);
+	}
+}
---
+++ Assets/Scripts/StartManger.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartManger : MonoBehaviour {
+
+	public void StartGame()
+	{
+		Application.LoadLevel("CongaScene");
+	}
+
+	public void RestartGame()
+	{
+		Application.LoadLevel("LaunchScene");
+	}
+}
